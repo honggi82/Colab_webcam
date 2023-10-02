9f68d273efb3da870a0d9d561aef3d10964aeb6a
Hong Gi Yeom <47119476+honggi82@users.noreply.github.com>
2023-10-03 01:45:39 +0900

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -4,7 +4,7 @@ from IPython.display import display, Javascript, Image
 from google.colab.output import eval_js
 from google.colab.patches import cv2_imshow
 from base64 import b64decode, b64encode
-#import cv2
+import cv2
 import numpy as np
 import PIL
 import io
